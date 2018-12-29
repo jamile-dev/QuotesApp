d822ab44bc964f7cb5b6e6249b487b19d4908d18
jamilelima <jamilesousalima@gmail.com>
2018-12-29 10:31:16 -0300

    Init integration of QuoteFragment with fetch data and fix layout margin and font size

--- app/src/main/java/io/github/jamilelima/quotesapp/MainActivity.java
+++ app/src/main/java/io/github/jamilelima/quotesapp/MainActivity.java
@@ -1,16 +1,35 @@
 package io.github.jamilelima.quotesapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import io.github.jamilelima.quotesapp.data.QuoteData;
+import io.github.jamilelima.quotesapp.data.QuoteViewPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+  private QuoteViewPagerAdapter quoteViewPagerAdapter;
+  private ViewPager viewPager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    quoteViewPagerAdapter = new QuoteViewPagerAdapter(getSupportFragmentManager(), getFragments());
+    viewPager = findViewById(R.id.viewPager);
+    viewPager.setAdapter(quoteViewPagerAdapter);
+  }
 
-    new QuoteData().getQuotes();
+  private List<Fragment> getFragments() {
+    List<Fragment> fragmentList = new ArrayList<>();
+
+    for(int i = 0; i < 5; i++) {
+      QuoteFragment quoteFragment = QuoteFragment.newInstance("Programmers are the best", "Jamile Lima");
+      fragmentList.add(quoteFragment);
+    }
+    return fragmentList;
   }
 }
--- app/src/main/java/io/github/jamilelima/quotesapp/QuoteFragment.java
+++ app/src/main/java/io/github/jamilelima/quotesapp/QuoteFragment.java
@@ -2,9 +2,11 @@ package io.github.jamilelima.quotesapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class QuoteFragment extends Fragment {
 
@@ -16,9 +18,30 @@ public class QuoteFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View quoteView = inflater.inflate(R.layout.fragment_quote, container, false);
+    TextView quoteText = quoteView.findViewById(R.id.quoteText);
+    TextView quoteAuthorName = quoteView.findViewById(R.id.authorName);
+    CardView cardView = quoteView.findViewById(R.id.cardContainer);
 
+    if (getArguments() != null) {
+      String quote = getArguments().getString("quote");
+      String author = getArguments().getString("author");
+      quoteText.setText(quote);
+      quoteAuthorName.setText(" - " + author);
+    } else {
+      quoteText.setText("");
+      quoteAuthorName.setText("");
+    }
 
     return quoteView;
+  }
+
+  public static QuoteFragment newInstance(String quote, String author) {
+    QuoteFragment fragment = new QuoteFragment();
+    Bundle bundle = new Bundle();
+    bundle.putString("quote", quote);
+    bundle.putString("author", author);
+    fragment.setArguments(bundle);
+    return fragment;
   }
 
 }
